25fa8b0b4925152645d7329331f69d6eaa0b358f
gaoxiang <sece1024@gmail.com>
2024-05-08 16:57:26 +0800

    feat: remove component from header bar

--- src/components/Header/DropdownUser.tsx
+++ src/components/Header/DropdownUser.tsx
@@ -44,7 +44,7 @@ const DropdownUser = () => {
       >
         <span className="hidden text-right lg:block">
           <span className="block text-sm font-medium text-black dark:text-white">
-            Thomas Anree
+            HGX
           </span>
           <span className="block text-xs">UX Designer</span>
         </span>
