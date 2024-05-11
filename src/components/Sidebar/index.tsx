754f34d77afaf01f5db7ac9f0b5e8417e9f68eb7
gaoxiang <sece1024@gmail.com>
2024-05-11 18:52:24 +0800

    feat: add deposit page

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -195,6 +195,16 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
                               expenses
                             </Link>
                           </li>
+                          <li>
+                            <Link
+                              href="/deposit"
+                              className={`group relative flex items-center gap-2.5 rounded-md px-4 font-medium text-bodydark2 duration-300 ease-in-out hover:text-white ${
+                                pathname === "/deposit" && "text-white"
+                              }`}
+                            >
+                              deposit
+                            </Link>
+                          </li>
                         </ul>
                       </div>
                       {/* <!-- Dropdown Menu End --> */}
