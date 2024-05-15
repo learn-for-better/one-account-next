dd6f3b299ad5e9694bdccbc9dc69d72150219072
gaoxiang <sece1024@gmail.com>
2024-05-15 10:32:48 +0800

    feat: Add lazy loading for DepositChart and DepositLineChart components

--- src/app/deposit/page.tsx
+++ src/app/deposit/page.tsx
@@ -1,37 +1,47 @@
 'use client';
-import DepositChart from "@/components/Charts/DepositChart";
-import DepositLineChart from "@/components/Charts/DepositLineChart";
 import DefaultLayout from "@/components/Layouts/DefaultLayout";
 import { Deposit } from "@/types/deposit";
 import api from "@/utils/api";
 import React, { useState, useEffect } from "react";
+import dynamic from 'next/dynamic'
+
+interface DepositPageProps {
+    depositMap: Map<string, Deposit[]>;
+}
+
+const DynamicChart = dynamic(() => import('@/components/Charts/DepositChart'), { ssr: false });
+const DynamicLineChart = dynamic(() => import('@/components/Charts/DepositLineChart'), { ssr: false });
+
+const MyDepositChart = ({depositMap}: DepositPageProps) => {
+    return (<div className="grid grid-cols-4 gap-4 md:gap-6 2xl:gap-7.5">
+        <DynamicChart depositMap={depositMap} />
+        <DynamicLineChart depositMap={depositMap} />
+    </div>);
+}
 
 const DepositPage: React.FC = () => {
-    const [depositMap, setDepositMap] = useState<Map<string, Deposit[]>>();
+    const [data, setData] = useState<Map<string, Deposit[]>>(new Map());
 
     useEffect(() => {
         api.getDeposits().then(response => {
             const deposits = response.data;
-            const depositsByDate: Map<string, Deposit[]> = new Map();
+            const depositMap: Map<string, Deposit[]> = new Map();
             deposits.forEach(deposit => {
                 const dateStr = new Date(deposit.date).toLocaleDateString('zh-CN', { year: 'numeric', month: '2-digit', day: '2-digit' });
-                if (!depositsByDate.has(dateStr)) {
-                    depositsByDate.set(dateStr, []);
+                if (!depositMap.has(dateStr)) {
+                    depositMap.set(dateStr, []);
                 }
-                depositsByDate.get(dateStr)?.push({ date: dateStr, amount: Number(deposit.amount), description: deposit.description });
+                depositMap.get(dateStr)?.push({ date: dateStr, amount: Number(deposit.amount), description: deposit.description });
             });
-            setDepositMap(depositsByDate);
+            setData(depositMap);
         }).catch(error => {
             console.error('Error fetching data:', error);
         });
-    }, [])
+    }, []);
 
     return (
         <DefaultLayout>
-            <div className="grid grid-cols-4 gap-4 md:gap-6 2xl:gap-7.5">
-            {depositMap && <DepositChart depositMap={depositMap} />}
-            {depositMap && <DepositLineChart depositMap={depositMap} />}
-            </div>
+           {typeof window !== "undefined" && data.size > 0 && <MyDepositChart depositMap={data}/>}
         </DefaultLayout>
     );
 }
